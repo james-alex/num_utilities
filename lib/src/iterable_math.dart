44c3a4ecc12f9effe45775734808e56b13aaa4e6
James Alexander <j.alex@email.com>
2021-03-15 22:16:42 -0700

    Update every method and getter to reference a single implementation across extensions.

--- lib/src/iterable_math.dart
+++ lib/src/iterable_math.dart
@@ -3,161 +3,76 @@ import 'num_utilities.dart';
 /// Extends `Iterable<double>` with additional basic methods.
 extension IterableMathDouble on Iterable<double> {
   /// Returns the sum of the values in the iterable.
-  double sum() => length >= 2
-      ? reduce((a, b) => a + b)
-      : length == 1
-          ? first
-          : 0.0;
+  double sum() => cast<num?>().sum().toDouble();
 
   /// Returns the sum of the absolute values in the iterable.
-  double absSum([bool invert = false]) {
-    var value = length >= 2
-        ? reduce((a, b) => a.abs() + b.abs())
-        : length == 1
-            ? first.abs()
-            : 0.0;
-    if (invert) value = value.invert;
-    return value;
-  }
+  double absSum([bool invert = false]) => cast<num?>().absSum(invert).toDouble();
 
   /// Returns the highest value in the list.
-  double get highest =>
-      reduce((value, element) => element > value ? element : value);
+  double get highest => cast<num?>().highest!.toDouble();
 
   /// Returns the lowest value in the list.
-  double get lowest =>
-      reduce((value, element) => element < value ? element : value);
+  double get lowest => cast<num?>().lowest!.toDouble();
 }
 
 extension IterableMathDoubleNullable on Iterable<double?> {
   /// Returns the sum of the values in the iterable.
-  double sum() => length >= 2
-      ? reduce((a, b) => (a ?? 0.0) + (b ?? 0.0))!
-      : length == 1
-          ? first ?? 0.0
-          : 0.0;
+  double sum() => cast<num?>().sum().toDouble();
 
   /// Returns the sum of the absolute values in the iterable.
-  double absSum([bool invert = false]) {
-    var value = length >= 2
-        ? reduce((a, b) => (a?.abs() ?? 0.0) + (b?.abs() ?? 0.0))!
-        : length == 1
-            ? first?.abs() ?? 0.0
-            : 0.0;
-    if (invert) value = value.invert;
-    return value;
-  }
+  double absSum([bool invert = false]) =>
+      cast<num?>().absSum(invert).toDouble();
 
   /// Returns the highest value in the list.
-  double? get highest => reduce((value, element) {
-        if (value == null || (element != null && element > value)) {
-          return element;
-        }
-        return value;
-      });
+  double? get highest => cast<num?>().highest?.toDouble();
 
   /// Returns the lowest value in the list.
-  double? get lowest => reduce((value, element) {
-        if (value == null || (element != null && element < value)) {
-          return element;
-        }
-        return value;
-      });
+  double? get lowest => cast<num?>().lowest?.toDouble();
 }
 
 /// Extends `Iterable<int>` with additional basic methods.
 extension IterableMathInt on Iterable<int> {
   /// Returns the sum of the values in the iterable.
-  int sum() => length >= 2
-      ? reduce((a, b) => a + b)
-      : length == 1
-          ? first
-          : 0;
+  int sum() => cast<num?>().sum().toInt();
 
   /// Returns the sum of the absolute values in the iterable.
-  int absSum([bool invert = false]) {
-    var value = length >= 2
-        ? reduce((a, b) => a.abs() + b.abs())
-        : length == 1
-            ? first.abs()
-            : 0;
-    if (invert) value = value.invert;
-    return value;
-  }
+  int absSum([bool invert = false]) => cast<num?>().absSum(invert).toInt();
 
   /// Returns the highest value in the list.
-  int get highest =>
-      reduce((value, element) => element > value ? element : value);
+  int get highest => cast<num?>().highest!.toInt();
 
   /// Returns the lowest value in the list.
-  int get lowest =>
-      reduce((value, element) => element < value ? element : value);
+  int get lowest => cast<num?>().lowest!.toInt();
 }
 
 /// Extends `Iterable<int>` with additional basic methods.
 extension IterableMathIntNullable on Iterable<int?> {
   /// Returns the sum of the values in the iterable.
-  int sum() => length >= 2
-      ? reduce((a, b) => (a ?? 0) + (b ?? 0))!
-      : length == 1
-          ? first ?? 0
-          : 0;
+  int sum() => cast<num?>().sum().toInt();
 
   /// Returns the sum of the absolute values in the iterable.
-  int absSum([bool invert = false]) {
-    var value = length >= 2
-        ? reduce((a, b) => (a?.abs() ?? 0) + (b?.abs() ?? 0))!
-        : length == 1
-            ? first?.abs() ?? 0
-            : 0;
-    if (invert) value *= -1;
-    return value;
-  }
+  int absSum([bool invert = false]) => cast<num?>().absSum(invert).toInt();
 
   /// Returns the highest value in the list.
-  int? get highest => reduce((value, element) {
-        if (value == null || (element != null && element > value)) {
-          return element;
-        }
-        return value;
-      });
+  int? get highest => cast<num?>().highest?.toInt();
 
   /// Returns the lowest value in the list.
-  int? get lowest => reduce((value, element) {
-        if (value == null || (element != null && element < value)) {
-          return element;
-        }
-        return value;
-      });
+  int? get lowest => cast<num?>().lowest?.toInt();
 }
 
 /// Extends `Iterable<num>` with additional basic methods.
 extension IterableMathNum on Iterable<num> {
   /// Returns the sum of the values in the iterable.
-  num sum() => length >= 2
-      ? reduce((a, b) => a + b)
-      : length == 1
-          ? first
-          : 0.0;
+  num sum() => cast<num?>().sum();
 
   /// Returns the sum of the absolute values in the iterable.
-  num absSum([bool invert = false]) {
-    var value = length >= 2
-        ? reduce((a, b) => a.abs() + b.abs())
-        : length == 1
-            ? first.abs()
-            : 0.0;
-    if (invert) value *= -1;
-    return value;
-  }
+  num absSum([bool invert = false]) => cast<num?>().absSum(invert);
 
   /// Returns the highest value in the list.
-  num get highest =>
-      reduce((value, element) => element > value ? element : value);
+  num get highest => cast<num?>().highest!;
 
   /// Returns the lowest value in the list.
-  num get lowest =>
-      reduce((value, element) => element < value ? element : value);
+  num get lowest => cast<num?>().lowest!;
 }
 
 /// Extends `Iterable<num>` with additional basic methods.
