34fe96936ea283b015bffa645b407b7f7ab8b3bd
James Alexander <contact@jamesalex.dev>
2022-07-11 02:52:35 -0700

    Add the [isWithin] and [isWithinRange] methods to [NumUtilities].

--- lib/src/num_utilities.dart
+++ lib/src/num_utilities.dart
@@ -17,6 +17,46 @@ extension NumUtilities on num {
     return this.roundTo(roundTo) == other.roundTo(roundTo);
   }
 
+  /// Returns `true` if this number matches or is within the
+  /// range of any of the parts defined in [expression].
+  ///
+  /// [expression] accepts a string of numbers and/or ranges
+  /// separated by commas.
+  ///
+  /// ```dart
+  ///   final value = 7;
+  ///   print(value.isWithin('0,2,4,6')); // false
+  ///   print(value.isWithin('1,2,6-8,10')); // true
+  ///   print(value.isWithin('7,10,12-15')); // true
+  /// ```
+  ///
+  /// Throws an [InvalidExpressionError] if [expression] isn't valid.
+  bool isWithin(String expression) {
+    final parts = expression.split(',');
+    for (var part in parts) {
+      if (part.contains('-')) {
+        final values =
+            part.split('-').map<num?>((value) => num.tryParse(value)).toList();
+        if (values.length != 2 || values.any((value) => value == null)) {
+          throw InvalidExpressionError(part);
+        }
+        if (isWithinRange(values[0]!, values[1]!)) return true;
+        continue;
+      }
+      final value = num.tryParse(part);
+      if (value == null) throw InvalidExpressionError(part);
+      if (this == value) return true;
+    }
+    return false;
+  }
+
+  /// Returns `true` if this number is within the range of [start] to [end].
+  bool isWithinRange(num start, num end) {
+    if (start <= end && this >= start && this <= end) return true;
+    if (start > end && this >= end && this <= start) return true;
+    return false;
+  }
+
   /// Rounds this number by [value]:
   /// ```dart
   /// (this * value).round() / value
@@ -50,4 +90,11 @@ extension DoubleUtilities on double {
 
   /// Flips the sign of this number.
   double get invert => this * -1;
+}
+
+/// An error thrown when an invalid expression is
+/// provided to the `isWithin` extension method.
+class InvalidExpressionError extends UnsupportedError {
+  InvalidExpressionError(String expression)
+      : super('\'$expression\' is not a valid expression.');
 }
--- test/num_utilities_test.dart
+++ test/num_utilities_test.dart
@@ -10,6 +10,46 @@ void main() {
       expect(numA.equals(numB, 100), equals(true));
     });
 
+    test('isWithin', () {
+      final number = 5.556;
+      expect(number.isWithin('5'), equals(false));
+      expect(number.isWithin('5.556'), equals(true));
+      expect(number.isWithin('5-6'), equals(true));
+      expect(number.isWithin('6-5'), equals(true));
+      expect(number.isWithin('4,5,6'), equals(false));
+      expect(number.isWithin('4, 5, 6'), equals(false));
+      expect(number.isWithin('4.556,5.556,6.556'), equals(true));
+      expect(number.isWithin('4.556, 5.556, 6.556'), equals(true));
+      expect(number.isWithin('2-4,5.556,6-8'), equals(true));
+      expect(number.isWithin('0-2,8-10,12-15'), equals(false));
+      expect(number.isWithin('0-2,4-8,10-12'), equals(true));
+      final integer = 5;
+      expect(integer.isWithin('5'), equals(true));
+      expect(integer.isWithin('5.556'), equals(false));
+      expect(integer.isWithin('5-6'), equals(true));
+      expect(integer.isWithin('6-5'), equals(true));
+      expect(integer.isWithin('4,5,6'), equals(true));
+      expect(integer.isWithin('4, 5, 6'), equals(true));
+      expect(integer.isWithin('4.556,5.556,6.556'), equals(false));
+      expect(integer.isWithin('4.556, 5.556, 6.556'), equals(false));
+      expect(integer.isWithin('2-4,5.556,6-8'), equals(false));
+      expect(integer.isWithin('0-2,8-10,12-15'), equals(false));
+      expect(integer.isWithin('0-2,4-8,10-12'), equals(true));
+    });
+
+    test('isWithinRange', () {
+      final number = 5.556;
+      expect(number.isWithinRange(0, 10), equals(true));
+      expect(number.isWithinRange(10, 0), equals(true));
+      expect(number.isWithinRange(0, -10), equals(false));
+      expect(number.isWithinRange(10, 20), equals(false));
+      final integer = 5;
+      expect(integer.isWithinRange(0, 10), equals(true));
+      expect(integer.isWithinRange(10, 0), equals(true));
+      expect(integer.isWithinRange(0, -10), equals(false));
+      expect(integer.isWithinRange(10, 20), equals(false));
+    });
+
     test('roundTo', () {
       final number = 5.556;
       expect(number.roundTo(10), equals(5.6));
